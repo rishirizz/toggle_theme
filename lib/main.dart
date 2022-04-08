3aa9c0c31bd8ae382da013362d780ca12ab93713
Arijeet <arijeet@sbox.ltd>
2022-04-08 10:15:42 +0530

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:toggle_theme/theme/toggle_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system, //will take the default system theme
+      theme: ToggleTheme.lightTheme,
+      darkTheme: ToggleTheme.darkTheme,
+      // home: const ToggleTheme(),
+    );
+  }
+}
---
+++ lib/theme/toggle_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ToggleTheme {
+  static final darkTheme = ThemeData();
+  static final lightTheme = ThemeData();
+}
